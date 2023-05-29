7cfe504061181e539b87722b05d3245a8f45840d
acatzk <joshuaimalay@gmail.com>
2023-05-29 18:51:59 +0800

    feat: format code + configure apollo sandbox

--- teamseas-api/src/app.module.ts
+++ teamseas-api/src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+
+import { AppService } from './app.service'
+import { AppController } from './app.controller'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql']
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
