c9ec2b8500b5dd8bc66b84c11137984fa6a21552
acatzk <joshuaimalay@gmail.com>
2023-05-29 19:13:16 +0800

    feat: config apollo setup and generate donation resource

--- teamseas-api/src/app.module.ts
+++ teamseas-api/src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
 
 import { AppService } from './app.service'
 import { AppController } from './app.controller'
+import { DonationsModule } from './donations/donations.module'
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       typePaths: ['./**/*.graphql']
-    })
+    }),
+    DonationsModule
   ],
   controllers: [AppController],
   providers: [AppService]
