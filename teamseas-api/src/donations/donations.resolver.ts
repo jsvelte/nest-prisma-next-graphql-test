f677768752324b9b8cf5915fee23b90a026ae6c1
acatzk <joshuaimalay@gmail.com>
2023-05-31 22:08:58 +0800

    feat: enable validation with class-validator

--- teamseas-api/src/donations/donations.resolver.ts
+++ teamseas-api/src/donations/donations.resolver.ts
@@ -3,13 +3,14 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql'
 
 import { OrderByParams } from './../graphql'
 import { DonationsService } from './donations.service'
+import { DonationCreateInput } from './../@generated/prisma-nestjs-graphql/donation/donation-create.input'
 
 @Resolver('Donation')
 export class DonationsResolver {
   constructor(private readonly donationsService: DonationsService) {}
 
   @Mutation('createDonation')
-  create(@Args('createDonationInput') createDonationInput: Prisma.DonationCreateInput) {
+  create(@Args('createDonationInput') createDonationInput: DonationCreateInput) {
     return this.donationsService.create(createDonationInput)
   }
 
